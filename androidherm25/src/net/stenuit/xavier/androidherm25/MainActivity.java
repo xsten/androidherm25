613cae6320291ebd792e569a732b5600e1cb0ad7
Xavier Stenuit <xsgit@herm25.com>
2014-01-05 10:38:24 +0100

    Event management - shows selected user on console

--- androidherm25/src/net/stenuit/xavier/androidherm25/MainActivity.java
+++ androidherm25/src/net/stenuit/xavier/androidherm25/MainActivity.java
@@ -1,15 +1,21 @@
 package net.stenuit.xavier.androidherm25;
 
 import net.stenuit.xavier.androidherm25.db.PolyfaceDatasource;
+import net.stenuit.xavier.androidherm25.db.RegisteredUser;
 import android.os.Bundle;
 import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemClickListener {
+	private static final String TAG="androidherm25";
 	private PolyfaceDatasource datasource;
 	Cursor cursor;
 	
@@ -32,6 +38,8 @@ public class MainActivity extends Activity {
 		
 		SimpleCursorAdapter adapter=new SimpleCursorAdapter(this, R.layout.userandname, cursor, columns, toViews,0);
 		userListView.setAdapter(adapter);
+		
+		userListView.setOnItemClickListener(this);
 	}
 
 	@Override
@@ -51,6 +59,20 @@ public class MainActivity extends Activity {
 	protected void onResume() {
 		datasource.open();
 		super.onResume();
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		Log.i(TAG,"AdapterView="+arg0);
+		Log.i(TAG,"View="+arg1);
+		Log.i(TAG,"expected view="+findViewById(R.id.userListView));
+		Log.i(TAG,"third argument (int)="+arg2);
+		Log.i(TAG,"fourth argument (long)="+arg3);
+		if(arg0==findViewById(R.id.userListView))
+		{
+			RegisteredUser ru=datasource.getRegisteredUser(arg3);
+			Log.i(TAG,"RegisteredUser="+ru);
+		}
 	}
 
 }
--- androidherm25/src/net/stenuit/xavier/androidherm25/db/PolyfaceDatasource.java
+++ androidherm25/src/net/stenuit/xavier/androidherm25/db/PolyfaceDatasource.java
@@ -1,11 +1,17 @@
 package net.stenuit.xavier.androidherm25.db;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import net.stenuit.xavier.androidherm25.SqliteCreator;
 
 public class PolyfaceDatasource {
+	private final String TAG="androidherm25";
 	private SqliteCreator dbHelper;
 	private SQLiteDatabase database;
 
@@ -24,5 +30,46 @@ public class PolyfaceDatasource {
 
 	public SQLiteDatabase getDatabase() {
 		return database;
+	}
+	
+	public RegisteredUser getRegisteredUser(long idRegisteredUser)
+	{
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+		if(database==null)return null;
+		Cursor c=database.query("RegisteredUsers",new String[]{"address","creationDate","firstName","forwardMail","idRegisteredUsers","Language","phone","surName","unixUser","updateDate"},
+				"idRegisteredUsers="+idRegisteredUser,
+				null,
+				null,null,null
+				);
+
+		RegisteredUser ret=null;
+		
+		if(c.getCount()==1)
+		{
+			try
+			{
+				c.moveToFirst();
+				ret=new RegisteredUser();
+				ret.setAddress(c.getString(0));
+				ret.setCreationDate(sdf.parse(c.getString(1)));
+				ret.setFirstName(c.getString(2));
+				ret.setForwardMail(c.getString(3));
+				ret.setIdRegisteredUser((int) c.getLong(4));
+				ret.setLanguage(c.getInt(5));
+				ret.setPhone(c.getString(6));
+				ret.setSurName(c.getString(7));
+				ret.setUnixUser(c.getString(8));
+				ret.setUpdateDate(sdf.parse(c.getString(9)));
+			} catch (ParseException e) {
+				Log.e(TAG,"Error - could not parse date : "+c.getString(1));
+			}
+
+		}
+		else
+		{
+			Log.e(TAG,"Problem - query returned "+c.getCount()+" rows");
+		}
+		
+		return ret;
 	}
 }
